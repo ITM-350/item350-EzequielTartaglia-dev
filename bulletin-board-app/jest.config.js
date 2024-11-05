eba5f2c549f3c17611337d9d2a51a0d8ced0e027
Ezequiel M. Tartaglia <96756615+EzequielTartaglia@users.noreply.github.com>
2024-11-05 11:35:19 -0300

    Update jest.config.js

--- bulletin-board-app/jest.config.js
+++ bulletin-board-app/jest.config.js
@@ -1,12 +1,18 @@
+// jest.config.js
 module.exports = {
+  collectCoverage: true, 
+  coverageDirectory: 'coverage', 
   coverageThreshold: {
-    // Set coverage threshold for all .js files in the src directory
+    
     './src/*.js': {
       lines: 80,
     },
-    // Set coverage threshold for all .js files in the backend directory
     './backend/*.js': {
       lines: 80,
     },
   },
+  collectCoverageFrom: [
+    'src/**/*.js', 
+    'backend/**/*.js', 
+  ],
 };
