1f1cfe778e7bdfd2c07a7009b7fc9e8a04689c0c
EzequielTartaglia <ezequieltartaglia@churchofjesuschrist.org>
2024-11-14 15:41:36 -0300

    terraform-changes

--- bulletin-board-app/tf/main.tf
+++ bulletin-board-app/tf/main.tf
@@ -9,21 +9,21 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "subnet_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
+  availability_zone       = "us-west-2b"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet_c" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-east-1c"
+  availability_zone       = "us-west-2c"
   map_public_ip_on_launch = true
 }
 
