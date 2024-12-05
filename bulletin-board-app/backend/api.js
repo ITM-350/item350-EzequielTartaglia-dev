2cd001006a8b7e0459fa5fe6d5825c431bd65cd1
Ezequiel M. Tartaglia <96756615+EzequielTartaglia@users.noreply.github.com>
2024-12-05 00:16:01 -0300

    Update api.js

--- bulletin-board-app/backend/api.js
+++ bulletin-board-app/backend/api.js
@@ -1,3 +1,23 @@
+var eventsData = [
+  { date: "2017-11-21", detail: "Linuxing in London", id: 1, title: "Docker Workshop" },
+  { date: "2017-11-21", detail: "WinOps London", id: 2, title: "WinOps #17" },
+  { date: "2017-11-13", id: 3, title: "Docker London" }
+];
+
+exports.events = function (req, res) {
+  res.json(eventsData); 
+};
+
+exports.event = function (req, res) {
+  const eventId = req.params.eventId; 
+  const event = eventsData[eventId]; 
+  if (event) {
+    res.json(event);
+  } else {
+    res.json(undefined); 
+  }
+};
+
 const statusCodes = require('http'). STATUS_CODES;
 const httpConstants = require('http2').constants;
 Cree una instancia de un cliente de documentos de DynamoDB con el SDK
